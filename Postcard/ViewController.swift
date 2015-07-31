466bc4d0b87a0ef0fed40e199a4e06817f05d5a3
yash1990 <WhiteTiger@Yashs-MacBook-Pro.local>
2015-07-30 18:35:27 -0700

    added a comment a to test commits

--- Postcard/ViewController.swift
+++ Postcard/ViewController.swift
@@ -27,6 +27,7 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+        // Adding a Comment to test commits
     }
 
     @IBAction func sendMailButtonPressed(sender: UIButton) {
